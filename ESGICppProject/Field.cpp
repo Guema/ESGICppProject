b86deea57af8afaf0c9daf9bc0107a2479c7623f
Guema <floorent1993@gmail.com>
2016-02-02 18:39:01 +0100

    Adding some things in Field

---
+++ ESGICppProject/Field.cpp
@@ -0,0 +1,16 @@
+#include "Field.h"
+
+Field::Field(int dimx, int dimy)
+{
+
+}
+
+Field::~Field()
+{
+
+}
+
+int Field::operator()(int posx, int posy)
+{
+	
+}
--- ESGICppProject/Field.h
+++ ESGICppProject/Field.h
@@ -1,1 +1,26 @@
 #pragma once
+
+enum Case
+{
+	Case_Empty,
+	Case_Building,
+	Case_Landing,
+	Case_Unreachable
+};
+
+class Field
+{
+protected:
+	int dimentions[2];
+	Case cases[];
+
+public:
+	Field(int dimx, int dimy);
+	virtual ~Field();
+
+	int GetWidth() { return dimentions[0]; }
+	int GetHeith() { return dimentions[1]; }
+
+	int operator()(int posx, int posy);
+
+};
